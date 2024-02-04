d2362c5441987b98fbf5aeb335c0f4cd727edd19
vargha1 <vargha.rahavi@gmail.com>
2024-02-04 16:14:00 +0330

    VR13

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 window.addEventListener('scroll', (event) => {
     if (document.body.scrollTop > 42 || document.documentElement.scrollTop > 42) {
         document.getElementById("header2").classList.remove("top-[42px]");
-        document.getElementById("logo").classList.remove("-top-[20px]");
-        document.getElementById("logo").classList.add("-top-[6px]");
         document.querySelector('#header2 nav').classList.add("top-[10px]");
     } else if ((document.body.scrollTop > 0 && document.body.scrollTop < 42) || (document.documentElement.scrollTop > 0 && document.documentElement.scrollTop < 42)) {
         document.getElementById("header2").classList.add("top-[42px]");
-        document.getElementById("logo").classList.add("-top-[20px]");
-        document.getElementById("logo").classList.remove("-top-[6px]");
         document.querySelector('#header2 nav').classList.remove("top-[10px]");
         document.getElementById("header2").classList.add("fixed");
     }
