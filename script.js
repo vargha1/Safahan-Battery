637b696bd69ef485c855a970186be3115e4f730c
vargha1 <vargha.rahavi@gmail.com>
2024-02-14 17:17:41 +0330

    VR23

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 window.addEventListener('scroll', (event) => {
     if (document.body.scrollTop > 42 || document.documentElement.scrollTop > 42) {
         document.getElementById("header2").classList.remove("top-[42px]");
-        document.querySelector('#header2 nav').classList.add("top-[10px]");
     } else if ((document.body.scrollTop > 0 && document.body.scrollTop < 42) || (document.documentElement.scrollTop > 0 && document.documentElement.scrollTop < 42)) {
         document.getElementById("header2").classList.add("top-[42px]");
-        document.querySelector('#header2 nav').classList.remove("top-[10px]");
         document.getElementById("header2").classList.add("fixed");
     }
 });
@@ -15,6 +13,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
         return false;
     }
+    function closeCanvas() {
+        offCanvas.classList.add('-right-[150%]');
+        cover.classList.add('hidden');
+        document.removeEventListener('wheel', preventScroll, { passive: false });
+    }
+    document.getElementById('p-link').addEventListener('click', () => {
+        window.scroll(0, 940);
+    });
     let offCanvasH = document.getElementById("offcanvasheader");
     let offCanvas = document.getElementById("offcanvas");
     let cover = document.getElementById('cover')
@@ -24,8 +30,9 @@ window.addEventListener('DOMContentLoaded', () => {
         document.addEventListener('wheel', preventScroll, { passive: false });
     });
     cover.addEventListener('click', () => {
-        offCanvas.classList.add('-right-[150%]');
-        cover.classList.add('hidden');
-        document.removeEventListener('wheel', preventScroll, { passive: false });
+        closeCanvas();
+    });
+    document.getElementById('p-link').addEventListener('click', () => {
+        closeCanvas();
     });
 });
